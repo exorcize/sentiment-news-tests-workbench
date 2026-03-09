445238b5a3cccd609a98b718d53cca151503b4ea
exorcize <fant9sy@proton.me>
2026-03-09 13:44:18 -0300

    bulk

--- test_converted_model.py
+++
@@ -1,8 +0,0 @@
-from optimum.onnxruntime import ORTModelForSequenceClassification
-from transformers import AutoTokenizer, pipeline
-
-tokenizer = AutoTokenizer.from_pretrained("./models/sentiment-onnx")
-model = ORTModelForSequenceClassification.from_pretrained("./models/sentiment-onnx")
-pipe = pipeline("text-classification", model=model, tokenizer=tokenizer)
-
-print(pipe("Apple beats earnings expectations."))
